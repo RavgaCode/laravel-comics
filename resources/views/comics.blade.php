de5895c7d29818f96a97fc69ece43fe8494f9ab4
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-08-26 20:14:45 +0200

    imported blue band

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -5,6 +5,7 @@
 @endsection
 
 @section('main_content')
+{{-- Comics list --}}
 <section class="comics-list">
     <div class="container">
       <div class="comics-wrapper">
@@ -16,6 +17,43 @@
           </div>
         @endforeach
       </div>
+    </div>
+  </section>
+  {{-- Blue band --}}
+  <section class="blue-band">
+    <div class="container">
+            <ul>
+                <li>
+                    <a href="#">
+                        <img src="img/buy-comics-digital-comics.png" alt="logo digital comics">
+                        <span>Digital comics</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="img/buy-comics-merchandise.png" alt="logo DC merchandise">
+                        <span>DC Merchandise</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="img/buy-comics-subscriptions.png" alt="logo subscriptions">
+                        <span>Subscription</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="img/buy-comics-shop-locator.png" alt="logo DC shop">
+                        <span>Comics Shop Locator</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="img/buy-dc-power-visa.svg" alt="logo power visa">
+                        <span>DC Power Visa</span>
+                    </a>
+                </li>
+            </ul>
     </div>
   </section>
 @endsection
